e679d7d413c8101cf3b16f74738c52a21c972561
Junaid PV <junu.pv@gmail.com>
2021-10-08 15:04:36 +0530

    Filter plugin to rotate images and alter entity embed dialog to give options to rotate images.

---
+++ src/Plugin/Filter/FilterRotate.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\media_extra\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Provides a filter to align elements.
+ *
+ * @Filter(
+ *   id = "me_filter_rotate",
+ *   title = @Translation("Rotate images"),
+ *   description = @Translation("Uses a <code>data-rotate</code> attribute on <code>&lt;img&gt;</code> tags to align images."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE
+ * )
+ */
+class FilterRotate extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    $result = new FilterProcessResult($text);
+
+    if (stristr($text, 'data-rotate') !== FALSE) {
+      $dom = Html::load($text);
+      $xpath = new \DOMXPath($dom);
+      foreach ($xpath->query('//*[@data-rotate]') as $node) {
+        // Read the data-align attribute's value, then delete it.
+        $rotate = $node->getAttribute('data-rotate');
+        $node->removeAttribute('data-rotate');
+
+        $rotate_caption = $node->getAttribute('data-rotate-caption');
+        $node->removeAttribute('data-rotate-caption');
+
+        // If one of the allowed alignments, add the corresponding class.
+        if (in_array($rotate, ['left', 'right'])) {
+          $classes = $node->getAttribute('class');
+          $classes = (strlen($classes) > 0) ? explode(' ', $classes) : [];
+          $classes[] = 'rotate-' . $rotate;
+
+          if (!empty($rotate_caption)) {
+            $classes[] = 'rotate-caption';
+          }
+          $node->setAttribute('class', implode(' ', $classes));
+        }
+
+      }
+      $result->setProcessedText(Html::serialize($dom));
+    }
+
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    if ($long) {
+      return $this->t('
+        <p>You can rotate images, videos, blockquotes and so on to the left, right or center. Examples:</p>
+        <ul>
+          <li>Rotate an image to the left: <code>&lt;img src="" data-rotate="left" /&gt;</code></li>
+          <li>Rotate an image to the right: <code>&lt;img src="" data-rotate="right" /&gt;</code></li>
+          <li>… and you can apply this to other elements as well: <code>&lt;video src="" data-rotate="left" /&gt;</code></li>
+        </ul>');
+    }
+    else {
+      return $this->t('You can rotate images (<code>data-rotate="left"</code>), but also videos, blockquotes, and so on.');
+    }
+  }
+
+}
